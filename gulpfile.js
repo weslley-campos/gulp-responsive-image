1f53ebf358bbeabc82abb3042e3d23d345f25ca1
Weslley Campos <weslley_campos@outlook.com>
2018-08-10 16:44:30 -0300

    Created tasks for resize

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 const gulp = require('gulp')
 const webp = require('gulp-webp')
+const clean = require('gulp-clean')
 const rename = require('gulp-rename')
 const resize = require('gulp-image-resize')
 const parallel = require('concurrent-transform')
 const os = require('os')
 
-gulp.task('default', function() {
-
+gulp.task('default', ['clean'], function() {
+    gulp.start('webp-convert')
+    gulp.start('imageResize85')
+    gulp.start('imageResize65')
+    gulp.start('imageResize50')
+    gulp.start('imageResize30')
 })
+
+gulp.task('imageResize85', imagesResize(85, "-1366"))
+gulp.task('imageResize65', imagesResize(65, "-1024"))
+gulp.task('imageResize50', imagesResize(50, "-768"))
+gulp.task('imageResize30', imagesResize(30, "-460"))
 
 function imagesResize(percent, sufixResolution) {
     gulp.src('SiteZeus/**/*.{png,jpg}')
@@ -24,7 +34,12 @@ function imagesResize(percent, sufixResolution) {
 }
 
 gulp.task('webp-convert', function () {
-    gulp.src('SiteZeus/**/*.{jpg,png}')
-        .pipe(webp({ quality: 100 }))
-        .pipe(gulp.dest('dist'))
+    return gulp.src('SiteZeus/**/*.{jpg,png}')
+            .pipe(webp({ quality: 100 }))
+            .pipe(gulp.dest('dist'))
+})
+
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(clean())
 })
